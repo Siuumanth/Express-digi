c8f89138fefb813cc488c9685fe7f695910400f6
Siuumanth <gsumanth3005@gmail.com>
2025-02-27 22:34:29 +0530

    added .env file and gitignore

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
+import 'dotenv/config'  //best way to import tis
 import express from 'express';
 const app = express();
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 //making routes
 app.get("/", (req,res) => {
